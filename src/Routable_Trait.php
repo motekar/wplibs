14b8c73a673ab4b1e71b93f35d3ce05a4597e706
fadlee <fad.lee@hotmail.com>
2020-04-03 00:00:40 +0700

    Initial commit

---
+++ src/Routable_Trait.php
@@ -0,0 +1,239 @@
+<?php
+namespace Motekar\WPLibs;
+
+/**
+ * Routable Trait
+ *
+ * Example of use:
+ *
+ * <code>
+ * // init routable in __construct
+ * $this->route_init( 'rootslug' );
+ *
+ * // register routes in route_register function
+ *
+ * $this->route( 'GET foo/{id}/{slug}', function( $id, $slug ) {
+ *     // available actions:
+ *     // - directly echo the output
+ *     // - return file path, will be included automatically
+ *     // - return array or object, will be sent as JSON using wp_send_json
+ *     // - return true // let WP process the request
+ * }, [
+ *     [ 'regex' => [ 'id' => '\d+' ] ] // by default the regex is [^/]+
+ * ] );
+ * </code>
+ *
+ * @author  Fadlul Alim <fad.lee@hotmail.com>
+ *
+ * @since 1.0.0
+ */
+trait Routable_Trait {
+	var $root_slug = '';
+	var $routes = array();
+
+	public function route_init( $root_slug ) {
+		$this->root_slug = $root_slug;
+
+		if ( preg_match("#^{$this->root_slug}#", $this->get_current_uri() ) ) {
+			$this->route_register();
+
+			if ( $this->route_resolve( false ) ) {
+				$this->register_route_filters();
+			}
+		}
+	}
+
+	private function register_route_filters() {
+		\add_action( 'after_setup_theme', function() {
+			if ( ! current_user_can( 'manage_options' ) && ! is_admin() ) {
+				show_admin_bar(false);
+			}
+			remove_action( 'wp_print_styles', 'print_emoji_styles' );
+			remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+		} );
+
+		\add_action( 'wp_enqueue_scripts', function() {
+			wp_dequeue_style( 'wp-block-library' );
+ 			wp_dequeue_style( 'wp-block-library-theme' );
+		} );
+
+		// Codes below are inspired from typerocket Routes.php
+		// template_include is needed to keep admin bar
+		\add_action( 'template_include', function( $template ) {
+			$return = $this->route_resolve();
+			return $template;
+		} );
+
+		// disable auto redirect to trailingslashed url
+		\add_filter( 'redirect_canonical', function( $redirect_url, $requested_url ) {
+			return false;
+		}, 10, 2);
+
+		// below is the code to disable unwanted SQL query
+		\add_filter( 'query_vars', function( $vars ) {
+			$vars[] = $this->root_slug . '_route_var';
+			return $vars;
+		} );
+
+		\add_action('option_rewrite_rules', function( $rules ) {
+			$add = [];
+			$key = '^' . $this->get_current_uri() . '/?$';
+			$add[$key] = "index.php?{$this->root_slug}_route_var=1";
+
+			if( is_array( $rules ) ) {
+				$rules = array_merge( $add, $rules );
+			} else {
+				$rules = $add;
+			}
+
+			return $rules;
+		} );
+
+		\add_filter( 'posts_request', function( $sql, $q ) {
+			/** @var WP_Query $q */
+			if ( $q->is_main_query() && !empty($q->query[$this->root_slug . '_route_var']) ) {
+				// disable row count
+				$q->query_vars['no_found_rows'] = true;
+
+				// disable cache
+				$q->query_vars['cache_results'] = false;
+				$q->query_vars['update_post_meta_cache'] = false;
+				$q->query_vars['update_post_term_cache'] = false;
+
+				\add_filter('body_class', function($classes) { array_push($classes, 'custom-route'); return $classes; });
+				return false;
+			}
+			return $sql;
+		}, 10, 3 );
+	}
+
+	function route( $path, $callback, $args = [] ) {
+		if ( preg_match( '#^(get|post|put|patch|delete) (.+)#i', $path, $matches ) ) {
+			$method = strtolower( $matches[1] );
+			$path = $matches[2];
+		} else {
+			$method = 'get';
+		}
+
+		$regex  = isset( $args['regex']) ? $args['regex'] : '';
+		$this->routes[] = compact( 'path', 'callback', 'method', 'regex' );
+	}
+
+	function route_resolve( $execute = true ) {
+		// parse request uri
+		$current_uri	= trim( preg_replace( "#^{$this->root_slug}/?#", '', $this->get_current_uri() ), '/' );
+		$current_method = strtolower( $_SERVER['REQUEST_METHOD'] );
+
+		// method spoofing, inspired by Laravel
+		if ( preg_match( '#^(put|patch|delete)#i', filter_input( INPUT_POST, '_method' ) ) ) {
+			$current_method = filter_input( INPUT_POST, '_method' );
+		}
+
+		$route_matched = '';
+		$route_params  = [];
+
+		foreach ( $this->routes as $route ) {
+			if ( $route['method'] != $current_method ) {
+				continue;
+			}
+
+			if ( $route['path'] == $current_uri ) {
+				$route_matched = $route;
+			} elseif ( strpos( $route['path'], '{' ) ) {
+				// route with parameter
+				list( $route_matched, $route_params ) =
+					$this->route_match_regex( $route, $current_uri );
+			}
+		}
+
+		if ( is_array( $route_matched ) ) {
+			if ( $execute ) {
+				$this->route_execute( $route_matched['callback'], $route_params );
+			} else {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	function route_match_regex( $route, $request_uri ) {
+		$route_matched = '';
+		$route_params = [];
+
+		// match route with parameters
+		if ( preg_match_all( '/\{(.*?)\}/', $route['path'], $matches ) ) {
+			// build regex
+			$regex = '#' . str_replace( '/', '\\/', $route['path'] ) . '#';
+			foreach ( $matches[1] as $param ) {
+				$route_regex = empty( $route['regex'][$param] ) ? '[^/]+' : $route['regex'][$param];
+				$regex = str_replace( '{'. $param . '}', '(' . $route_regex . ')', $regex );
+			}
+
+			// match regex with request uri
+			if ( preg_match( $regex, $request_uri, $route_params ) ) {
+				$route_matched = $route;
+				array_shift( $route_params );
+			}
+		}
+		return array( $route_matched, $route_params );
+	}
+
+	function route_execute( $callback, $params ) {
+		ob_start();
+		$return = call_user_func_array( $callback, $params );
+		$buffer = ob_get_clean();
+
+		// return is template file
+		if ( is_string( $return ) ) {
+			if ( file_exists( $return ) ) {
+				include $return;
+			} else {
+				echo $return;
+			}
+		}
+
+		if ( is_array( $return ) || is_object( $return ) ) {
+			\wp_send_json( $return );
+		}
+
+		echo $buffer;
+
+		if ( $return === true ) {
+			// don't exit, allow WP to run
+		} else {
+			exit();
+		}
+	}
+
+	function route_for_user( $redirect_to = '/login' ) {
+		if ( ! \is_user_logged_in() ) {
+			\wp_redirect( $this->get_url( $redirect_to ) );
+			exit;
+		}
+	}
+
+	function route_for_non_user( $redirect_to = '/' ) {
+		if ( is_user_logged_in() ) {
+			\wp_redirect( $this->get_url( $redirect_to ) );
+			exit;
+		}
+	}
+
+	function get_url( $path = '/' ) {
+		$path = ltrim( $path, '/' );
+		return \home_url( "/{$this->root_slug}/{$path}" );
+	}
+
+	function get_current_uri() {
+		return \untrailingslashit( ltrim( explode('?', $_SERVER['REQUEST_URI'] )[0], '/' ) );
+	}
+
+	function allowed_or_redirect( $redirect_to = '/login' ) {
+		if ( ! \is_user_logged_in() ) {
+			\wp_redirect( $this->get_url( $redirect_to ) );
+			exit;
+		}
+		return true;
+	}
+}
---
+++ src/Settings_Page.php
@@ -0,0 +1,227 @@
+<?php
+namespace Motekar\WPLibs;
+
+class Settings_Page {
+	public $page_name;
+	public $option_name;
+	public $settings;
+
+	public function __construct( $page_name, $option_name = '' ) {
+		$this->page_name   = $page_name;
+		$this->option_name = empty( $option_name ) ? $page_name : $option_name;
+
+		\add_action( 'admin_init', array( $this, 'init_settings' ) );
+		\add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+
+		\add_action( 'plugins_loaded', array( $this, 'save_options' ) );
+	}
+
+	public function init_settings() {
+		$this->settings = \apply_filters( $this->page_name . '_array', $this->_default_settings() );
+
+		// save initial settings
+		if ( false === get_option( $this->option_name, false ) ) {
+			$initial_options = [];
+			foreach ( $this->settings as $page_name => $page ) {
+				foreach ( $page['sections'] as $section_name => $section ) {
+					foreach ( $section['fields'] as $field_name => $field ) {
+						$initial_options[$field_name] = $field['default'] ?? '';
+					}
+				}
+			}
+			\update_option( $this->option_name, $initial_options );
+		}
+
+		$this->_register_settings();
+	}
+
+	private function _default_settings() {
+		return [
+			'tab-1' => [
+				'sections' => [
+					'basic' => [
+						'label' => 'Basic Fields',
+						'fields' => [
+							'text' => [
+								'type' => 'text',
+								'label' => 'Text Field'
+							],
+							'textarea' => [
+								'type' => 'textarea',
+								'label' => 'Textarea Field'
+							],
+							'select' => [
+								'type'          => 'select',
+								'label'         => 'Select Field',
+								'default'       => '',
+								'placeholder'   => '-- Select Option --',
+								'options'       => [
+									'option-1' => 'Option 1',
+									'option-2' => 'Option 2',
+									'option-3' => 'Option 3',
+									'option-4' => 'Option 4',
+								],
+								'desc'          => 'This is select field',
+							],
+							'radio' => [
+								'type'         => 'radio',
+								'label'        => 'Radio Field',
+								'default'      => 'option-1',
+								'options'      => [
+									'option-1' => 'Option 1',
+									'option-2' => 'Option 2',
+									'option-3' => 'Option 3',
+									'option-4' => 'Option 4',
+								],
+								'desc'         => 'This is radio field',
+							],
+							'checkbox' => [
+								'type'         => 'checkbox',
+								'label'        => 'Checkbox Field',
+								'label_title'  => 'Enable',
+								'default'      => '1',
+								'desc'         => 'This is checkbox field',
+							],
+							'checkboxes' => [
+								'type'         => 'checkbox',
+								'label'        => 'Checkbox Field (multiple)',
+								'default'      => 'option-1',
+								'options'      => [
+									'option-1' => 'Option 1',
+									'option-2' => 'Option 2',
+									'option-3' => 'Option 3',
+									'option-4' => 'Option 4',
+								],
+								'desc'         => 'This is checkbox field with multiple checkboxes',
+							],
+						]
+					]
+				]
+			],
+			'tab-2' => [
+				'sections' => [
+					'advanced' => [
+						'label' => 'Advanced Fields',
+						'fields' => [
+							'media' => [
+								'type' => 'media',
+								'label' => 'Media Field'
+							],
+							'banks' => [
+								'type' => 'bank-accounts',
+								'label' => 'Bank Accounts'
+							]
+						]
+					]
+				]
+			]
+		];
+	}
+
+	private function _register_settings() {
+		foreach ( $this->settings as $page_name => $page ) {
+			if ( ! isset( $page['sections'] ) ) {
+				continue;
+			}
+
+			foreach ( $page['sections'] as $section_name => $section ) {
+				\add_settings_section(
+					$section_name, // id
+					$section['label'] ?? '', // title
+					null,  // callback
+					$page_name // page
+				);
+
+				if ( ! isset( $section['fields'] ) ) {
+					continue;
+				}
+
+				foreach ( $section['fields'] as $field_name => $field ) {
+					$field['id']          = "setting_{$section_name}_{$field_name}";
+					$field['name']        = $field_name;
+					$field['description'] = $field['desc'] ?? '';
+					$field['class']       = empty( $field['class'] ) ? '' : $field['class'];
+
+					$long_field_name = "{$this->page_name}_{$field_name}";
+
+					\add_settings_field(
+						$long_field_name,  // id
+						$field['label'],  // title
+						array( $this, 'render_field' ), // callback
+						$page_name,   // page
+						$section_name,  // section
+						$field // args
+					);
+					\register_setting(
+						$this->page_name, // option group
+						$long_field_name // option name
+					);
+
+				}
+			}
+		}
+	}
+
+	private function _get_option( $option, $default = false ) {
+		$options = \get_option( $this->option_name, $default );
+
+		return $options[$option] ?? $default;
+	}
+
+	public function enqueue_scripts() {
+		if ( filter_input( INPUT_GET, 'page') != $this->page_name ) {
+			return;
+		}
+
+		\wp_enqueue_media();
+		\wp_enqueue_script( 'alpinejs', 'https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.js', array(), '2.x.x' );
+	}
+
+	public function render_page() {
+		derma_view( 'settings/page', [
+			'page_name'   => $this->page_name,
+			'option_name' => $this->option_name,
+		] );
+	}
+
+	public function render_field( $args ) {
+		$args['value']    =  $this->_get_option( $args['name'] );
+		$args['raw_name'] = $args['name'];
+		$args['name']     = "{$this->page_name}[{$args['name']}]";
+
+		derma_view( 'settings/fields/' . $args['type'], $args );
+	}
+
+	public function save_options() {
+		if ( filter_input( INPUT_POST, 'option_page' ) != $this->page_name ) {
+			return;
+		} else {
+			check_admin_referer( $this->page_name . '-options' );
+		}
+
+		$is_ajax = 'xmlhttprequest' == strtolower( $_SERVER['HTTP_X_REQUESTED_WITH'] ?? '' );
+
+		$options = filter_input( INPUT_POST, $this->page_name, FILTER_DEFAULT, FILTER_REQUIRE_ARRAY );
+		$options = $options ?? []; // must be an array
+		$options = apply_filters( $this->page_name . '_input', $options );
+		update_option( $this->option_name, $options );
+
+		if ( $is_ajax ) {
+			wp_send_json_success( [
+				'msg' => __('Settings Updated', 'derma')
+			] );
+			exit;
+		}
+
+		// // allow core wp to save the data
+		// foreach ( $options as $option_key => $option_value ) {
+		// 	$_POST[$this->page_name . '_' . $option_key] = $option_value;
+		// }
+		// return;
+
+		// Redirect back to the settings page that was submitted.
+		$goback = add_query_arg( 'settings-updated', 'true', wp_get_referer() );
+		wp_redirect( $goback );
+		exit;
+	}
+}
